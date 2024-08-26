6f06a07afc3985e20e5af6a6d0294b55062e628b
GuilhermeFio <ra48475438822@acaonsfatima.org.br>
2024-08-26 11:56:22 -0300

    adicionado as comps do 'cabeçalho' e  do 'card'

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,57 +2,99 @@ import './index.scss'
 import { Link } from 'react-router-dom'
 
 export default function Home(){
+
+    let cards= [
+        {
+        "link_card":'/ex1',
+        "cor_card": 'amarelo',
+        "titulo_card":'Cupom de Desconto ',
+        "subtitulo_card": 'Exercício 1' 
+    },
+    {
+        "link_card":'/ex2',
+        "cor_card": 'verde',
+        "titulo_card":'Converter KG/Gramas ',
+        "subtitulo_card": 'Exercício 2' 
+    },
+    {
+        "link_card":'/ex3',
+        "cor_card": 'ciano',
+        "titulo_card":'Valor total por quantidade ',
+        "subtitulo_card": 'Exercício 3' 
+    },
+    {
+        "link_card":'/ex10',
+        "cor_card": 'roxo',
+        "titulo_card":'Cálculo de IMC ',
+        "subtitulo_card": 'Exercício 10' 
+    },
+    {
+        "link_card":'/ex11',
+        "cor_card": 'vermelho',
+        "titulo_card":'Tabuada ',
+        "subtitulo_card": 'Exercício 11' 
+    }
+]
+
     return(
         <div className='home-page'>
-            <header className='cabecalho'>
-                
-                <img src='/assets/images/freimage.png'/>
-                <h1>React FreiS</h1>
-                
-                <div className='opcoes'> 
-                <p><Link to={'/'}>Home</Link></p>
-                <p><Link to={'/sobre'}>About</Link></p>
-                </div>
+          
+          <Cabecalho/>
                    
-                
-
-            </header>
             <section className='secao1'>
                 <h1>Escolha um treino...</h1>
+
+               
                 <div>
-                    <div className='um'>
-                        <div className='amarelo'></div>
-                        <h2>Cupom de desconto</h2>
-                        <p><Link className='ex1' to={'/ex1'}> Exercício 1 </Link> </p>
-                    </div>
-
-                    <div className='dois'>
-                        <div className='verde'></div>
-                        <h2>Converter Kg/gramas</h2>
-                        <p><Link className='ex2' to={'/ex2'}> Exercício 2 </Link></p>
-                    </div>
-
-                    <div className='tres'>
-                        <div className='ciano'></div>
-                        <h2>Valor total por quantidade</h2>
-                        <p><Link className='ex3' to={'/ex3'}> Exercício 3 </Link></p>
-                    </div>
 
 
-                    <div className='dez'>
-                        <div className='roxo'></div>
-                        <h2>Cálculo de IMC</h2>
-                        <p><Link className='ex10' to={'/ex10'}> Exercício 10 </Link></p>
-                    </div>
-
-                    <div className='onze'>
-                        <div className='vermelho'></div>
-                        <h2>Tabuada</h2>
-                        <p><Link className='ex11' to={'/ex11'}> Exercício 11 </Link> </p>
-                    </div>
+                {
+    cards.map(card =>
+        <Card
+            link_card = {card.link_card}
+            cor_card = {card.cor_card}
+            titulo_card = {card.titulo_card}
+            subtitulo_card = {card.subtitulo_card}
+        /> 
+    )
+    }  
                 </div>
 
             </section>
         </div>
     )
 }
+
+function Cabecalho(){
+    return(
+        <header className='cabecalho'>
+                
+        <img src='/assets/images/freimage.png'/>
+        <h1>React FreiS</h1>
+        
+        <div className='opcoes'> 
+        <p><Link to={'/'}>Home</Link></p>
+        <p><Link to={'/sobre'}>About</Link></p>
+        </div>
+           
+    </header>
+    )
+}
+
+
+    
+    function Card({link_card, cor_card, titulo_card, subtitulo_card}){
+
+    return(
+        <Link to = {link_card} className='link'>
+        <div className='card'>
+        
+                        <div className={`retangulo ${cor_card}`}></div>
+                        <h2>{titulo_card}</h2>
+                        <p> {subtitulo_card} </p>
+                        
+                    </div>
+                    </Link>
+    )
+}
+ 
